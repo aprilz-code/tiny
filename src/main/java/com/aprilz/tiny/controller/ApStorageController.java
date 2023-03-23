f4e3a549c305c380bce9b86d1de8526dc74289ac
lsh <liushaohui@163.com>
2023-03-23 20:03:21 +0800

    文件存储

---
+++ src/main/java/com/aprilz/tiny/controller/ApStorageController.java
@@ -0,0 +1,114 @@
+package com.aprilz.tiny.controller;
+
+import com.aprilz.tiny.common.api.CommonResult;
+import com.aprilz.tiny.common.storage.StorageService;
+import com.aprilz.tiny.mall.utils.CharUtil;
+import com.aprilz.tiny.mbg.entity.ApStorage;
+import com.aprilz.tiny.service.IApStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+/**
+ * <p>
+ * 文件存储表 前端控制器
+ * </p>
+ *
+ * @author aprilz
+ * @since 2022-07-20
+ */
+@RestController
+@RequestMapping("/storage")
+public class ApStorageController {
+
+
+    @Autowired
+    private StorageService storageService;
+    @Autowired
+    private IApStorageService apStorageService;
+
+    private String generateKey(String originalFilename) {
+        int index = originalFilename.lastIndexOf('.');
+        String suffix = originalFilename.substring(index);
+
+        String key = null;
+        ApStorage storageInfo = null;
+
+        do {
+            key = CharUtil.getRandomString(20) + suffix;
+            storageInfo = apStorageService.findByKey(key);
+        }
+        while (storageInfo != null);
+
+        return key;
+    }
+
+
+    @PostMapping("/upload")
+    public CommonResult upload(@RequestParam("file") MultipartFile file) throws IOException {
+        String originalFilename = file.getOriginalFilename();
+        ApStorage litemallStorage = storageService.store(file.getInputStream(), file.getSize(), file.getContentType(), originalFilename);
+        return CommonResult.success(litemallStorage);
+    }
+
+
+    /**
+     * 访问存储对象
+     *
+     * @param key 存储对象key
+     * @return
+     */
+    @GetMapping("/fetch/{key:.+}")
+    public ResponseEntity<Resource> fetch(@PathVariable String key) {
+        ApStorage apStorage = apStorageService.findByKey(key);
+        if (key == null) {
+            return ResponseEntity.notFound().build();
+        }
+        if (key.contains("../")) {
+            return ResponseEntity.badRequest().build();
+        }
+        String type = apStorage.getType();
+        MediaType mediaType = MediaType.parseMediaType(type);
+
+        Resource file = storageService.loadAsResource(key);
+        if (file == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().contentType(mediaType).body(file);
+    }
+
+    /**
+     * 访问存储对象
+     *
+     * @param key 存储对象key
+     * @return
+     */
+    @GetMapping("/download/{key:.+}")
+    public ResponseEntity<Resource> download(@PathVariable String key) {
+        ApStorage litemallStorage = apStorageService.findByKey(key);
+        if (key == null) {
+            return ResponseEntity.notFound().build();
+        }
+        if (key.contains("../")) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        String type = litemallStorage.getType();
+        MediaType mediaType = MediaType.parseMediaType(type);
+
+        Resource file = storageService.loadAsResource(key);
+        if (file == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().contentType(mediaType).header(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
+
+
+}
